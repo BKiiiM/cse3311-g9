df3edd746ae8da9909e744f7ca3f6ab60c497d4f
Kerry Pacheco <kerry.pachecogarcia@mavs.uta.edu>
2023-09-24 14:33:03 -0500

    Added functionality to "search by name" button so it opens the new activity screen with the input text box

--- app/src/main/java/com/example/recipegeneratorapplication/MainMenuActivity.java
+++ app/src/main/java/com/example/recipegeneratorapplication/MainMenuActivity.java
@@ -2,13 +2,38 @@ package com.example.recipegeneratorapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainMenuActivity extends AppCompatActivity {
 
+    Button search_button;
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        search_button = (Button) findViewById(R.id.search_by_name);
+
+        search_button.setOnClickListener(this::next_screen);
+
     }
+    private void next_screen(View view)
+    {
+        Intent myIntent = new Intent(MainMenuActivity.this, search_by_name_activity.class);
+        MainMenuActivity.this.startActivity(myIntent);
+    }
+
 }
