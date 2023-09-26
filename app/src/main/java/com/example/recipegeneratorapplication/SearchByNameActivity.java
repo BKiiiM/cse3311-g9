37d7656404f2f8c866934615a4aea3636c64bc79
Kerry Pacheco <kerry.pachecogarcia@mavs.uta.edu>
2023-09-25 22:08:14 -0500

    Fixed imports needed

--- app/src/main/java/com/example/recipegeneratorapplication/SearchByNameActivity.java
+++ app/src/main/java/com/example/recipegeneratorapplication/SearchByNameActivity.java
@@ -3,6 +3,8 @@ package com.example.recipegeneratorapplication;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
