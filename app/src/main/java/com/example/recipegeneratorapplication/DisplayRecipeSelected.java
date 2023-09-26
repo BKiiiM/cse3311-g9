b601777ec40f6f185740e7eb3898398e8e8ce8fd
Kerry Pacheco <kerry.pachecogarcia@mavs.uta.edu>
2023-09-25 22:49:12 -0500

    added background to "display recipe" screen

--- app/src/main/java/com/example/recipegeneratorapplication/DisplayRecipeSelected.java
+++ app/src/main/java/com/example/recipegeneratorapplication/DisplayRecipeSelected.java
@@ -1,5 +1,9 @@
 package com.example.recipegeneratorapplication;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +17,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -22,7 +28,25 @@ public class DisplayRecipeSelected extends AppCompatActivity
 {
     TextView selectedRecipeId;
     TextView recipeTitle;
+    ImageView recipePhoto;
     private static final String API_KEY = "7bc4cc04deb34da4b63e486dd734ca93";
+    public static Bitmap getBitmapFromURL(String src) {
+        try {
+            Log.e("src", src);
+            URL url = new URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            Log.e("Bitmap", "returned");
+            return myBitmap;
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("Exception", e.getMessage());
+            return null;
+        }
+    }
     private class recipeSearchById extends AsyncTask<Integer, Void, JSONObject> {
         @Override
         protected JSONObject doInBackground(Integer... params) {
@@ -66,10 +90,11 @@ public class DisplayRecipeSelected extends AppCompatActivity
         @Override
         protected void onPostExecute(JSONObject result) {
             super.onPostExecute(result);
-            //updating UI with data from results of JSONObject
+            //updating UI with data from results of JSONObject all fields from API are set here
 
             try {
                 recipeTitle.setText(result.getString("title"));
+                //recipePhoto.setImageBitmap(getBitmapFromURL(result.getString("image")));
             } catch (JSONException e) {
                 throw new RuntimeException(e);
             }
@@ -86,6 +111,7 @@ public class DisplayRecipeSelected extends AppCompatActivity
         int id = intent.getIntExtra("id",0);
         selectedRecipeId = findViewById(R.id.recipe_API_id);
         recipeTitle = findViewById(R.id.recipeTitle);
+        recipePhoto = findViewById(R.id.recipe_photo);
         //displays the recipe id on screen
         selectedRecipeId.setText(id+"");
         new recipeSearchById().execute(id);
